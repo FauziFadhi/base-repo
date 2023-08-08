4435326bfade3b70458fd1a2e1c7e7049937c6eb
Fauzi Fadhillah <fauzifadhi@rollingglory.com>
2023-08-08 13:28:05 +0700

    add Factory method name

--- src/sequelize-cache/sequelize-cache.module-definition.ts
+++ src/sequelize-cache/sequelize-cache.module-definition.ts
@@ -2,4 +2,4 @@ import { ConfigurableModuleBuilder } from "@nestjs/common";
 import { SequelizeCacheModuleOptions } from "./module-options.interface";
 
 export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN, ASYNC_OPTIONS_TYPE, OPTIONS_TYPE } =
-  new ConfigurableModuleBuilder<SequelizeCacheModuleOptions>().build();
+  new ConfigurableModuleBuilder<SequelizeCacheModuleOptions>().setFactoryMethodName('SequelizeConfigOptions').build();
